f99534cfbcb0119308580566eaea768d591a553b
Lip Ferreira <felipeferreirareis1@gmail.com>
2023-12-19 16:49:24 -0300

    Commit3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,9 @@ import Link from "next/link";
 import styles from "./homepage.module.css";
 
 export default function Home() {
-  return <div>
-    <Link href="/"></Link>
-  </div>;
+  return (
+    <div>
+      
+    </div>
+  );
 }
